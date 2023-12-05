538facf4908ba901b700dbac332204cf96db3725
Yannsean22 <69013397+Yannsean22@users.noreply.github.com>
2023-12-05 11:55:36 -0500

    Add files via upload

--- modules/settings/about/aboutConfig.py
+++ modules/settings/about/aboutConfig.py
@@ -1,1 +1,5 @@
+import modules.inputs.windowCanvas as windowCanvas
 
+def showAboutConfig():
+    About = ["Name: CUBEX MIRROR", "User: Meredith Teuber", "OS Version: 0.0.1", "Charles Version: 0.0.1","Production #: 0001 --dev","--0.0.1 in development","Crystal Ice"]
+    x = windowCanvas.windowCanvas('list',About, "Abouts")
